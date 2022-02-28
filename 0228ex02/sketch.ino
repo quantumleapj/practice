e441fcd9a67f87dc6ee4e88f86fe05c5aaf2bb15
JHJ <90052524+quantumleapj@users.noreply.github.com>
2022-02-28 13:22:13 +0900

    arduino

--- 0228ex02/sketch.ino
+++ 0228ex02/sketch.ino
@@ -1,9 +1,19 @@
+const int pu_led_pin=3;
+const int pd_led_pin=4;
+
+
 void setup()
 {
+  pinMode(pu_led_pin,OUTPUT);
+  pinMode(pd_led_pin,OUTPUT);
 	
 }
 
 void loop()
 {
-	
+	digitalWrite(pd_led_pin,HIGH);
+	digitalWrite(pu_led_pin,HIGH);
+
+	// digitalWrite(pd_led_pin,LOW);
+	// digitalWrite(pu_led_pin,LOW);
 }
